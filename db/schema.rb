d9f9e0c61d8a61087fed6e8ce2a020f095fd2c23
harivmasoor <harimasoor@gmail.com>
2023-08-18 15:29:17 -0700

    title requirement not needed anymore

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_08_18_002621) do
+ActiveRecord::Schema[7.0].define(version: 2023_08_18_222320) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -43,7 +43,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_18_002621) do
   end
 
   create_table "posts", force: :cascade do |t|
-    t.string "title", null: false
+    t.string "title"
     t.text "body"
     t.bigint "user_id"
     t.datetime "created_at", null: false
