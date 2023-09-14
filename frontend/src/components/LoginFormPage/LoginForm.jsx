192508083259a948b4baf0985b18767fa7c46769
harivmasoor <harimasoor@gmail.com>
2023-09-14 12:40:04 -0700

    commented out seed

--- frontend/src/components/LoginFormPage/LoginForm.jsx
+++ frontend/src/components/LoginFormPage/LoginForm.jsx
@@ -1,106 +1,93 @@
-import React, { useState, useRef } from "react";
-import { Link, useHistory, Redirect } from 'react-router-dom';
-import * as sessionActions from "../../store/session";
-import { useDispatch } from "react-redux";
-import { useSelector } from 'react-redux';
-import "./LoginForm.css";
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { Redirect } from 'react-router-dom'; // <-- Add this import
+import './SessionForm.css';
 
+import { login, clearSessionErrors } from '../../store/session';
 
-function LoginForm({ setShowModal, setShowSignupModal }) { 
-    const dispatch = useDispatch();
-    const history = useHistory();
-    const [credential, setCredential] = useState("");
-    const [password, setPassword] = useState("");
-    const [showPassword, setShowPassword] = useState(false); 
-    const credentialRef = useRef();
-    const passwordRef = useRef();
-    const sessionUser = useSelector(state => state.session.user);
-    const sessionErrors = useSelector(state => state.errors.session)// for session errors
-    if (sessionUser) {
-      return <Redirect to='/news_feed' />
-    }
-
-    const handleCredentialChange = (e) => {
-        setCredential(e.target.value);
-        if(!e.target.value) e.target.placeholder = " ";
-    }
-
-    const handlePasswordChange = (e) => {
-        setPassword(e.target.value);
-        if(!e.target.value) e.target.placeholder = " ";
-    }
-
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        dispatch(sessionActions.login({ credential, password }))
+class LoginForm extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: '',
+      password: '',
+      shouldRedirect: false // <-- Add this line
     };
+    this.update = this.update.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
 
-    const handleDemoLogin = () => {
-        setCredential("July4th@1776.com");
-        setPassword("password");
-        credentialRef.current.focus();
-        setTimeout(() => passwordRef.current.focus(), 100);
+  componentWillUnmount() {
+    this.props.clearSessionErrors();
+  }
+
+  update(field) {
+    return e => this.setState({ [field]: e.currentTarget.value });
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    const { email, password } = this.state;
+    this.props.login({ email, password }).then(success => {
+      if (success) {
+        this.setState({ shouldRedirect: true }); // <-- Add this line
       }
+    });
+  }
+
+  render() {
+    // Redirect if login is successful
+    if (this.state.shouldRedirect) {
+      return <Redirect to="/" />; // <-- Add this line
+    }
+
+    const { errors } = this.props;
+    const { email, password } = this.state;
 
     return (
-        <div className="login-form-container">
-            <form className="logInForm" onSubmit={handleSubmit}>
-            <h1>Welcome Back</h1>
-            <div className={`error-container ${sessionErrors?.length ? 'active' : ''}`}>
-                {sessionErrors?.map((error) => <span key={error} className="error-item">{error}</span>)}
-            </div>
-                <div className="input-container">
-                    <input
-                        ref={credentialRef}
-                        type="text"
-                        value={credential}
-                        onChange={handleCredentialChange}
-                        required
-                        id="credential-input"
-                        placeholder=""
-                    />
-                    <label htmlFor="credential-input">name, phone, email</label>
-                </div>
-                <div className="input-container">
-                    <input
-                        ref={passwordRef}
-                        type={showPassword ? "text" : "password"}
-                        value={password}
-                        onChange={handlePasswordChange}
-                        required
-                        id="password-input"
-                        placeholder=""
-                    />
-                    <label htmlFor="password-input">Password</label>
-                    <button 
-                        type="button" 
-                        className="toggle-password"
-                        onClick={() => setShowPassword(prev => !prev)}
-                    >
-                        {showPassword ? '🙈' : '👁'}
-                    </button>
-                </div>
-                <div className="button-container">
-                <button type="submit">Log In</button>
-                </div>
-                <div className="or-separator">
-                        OR
-                    </div>
-
-
-                <div className="demo-container">
-                    <button onClick={handleDemoLogin}>George Washington</button>
-                </div>
-                <div className="signup-link">
-                New to Localvoices? 
-                <Link to="/"> Sign up</Link>
-            </div>
-            </form>
-        </div>
+      <form className="session-form" onSubmit={this.handleSubmit}>
+        <h2>Log In Form</h2>
+        <div className="errors">{errors?.email}</div>
+        <label>
+          <span>Email</span>
+          <input
+            type="text"
+            value={email}
+            onChange={this.update('email')}
+            placeholder="Email"
+          />
+        </label>
+        <div className="errors">{errors?.password}</div>
+        <label>
+          <span>Password</span>
+          <input
+            type="password"
+            value={password}
+            onChange={this.update('password')}
+            placeholder="Password"
+          />
+        </label>
+        <input
+          type="submit"
+          value="Log In"
+          disabled={!email || !password}
+        />
+      </form>
     );
+  }
 }
 
-export default LoginForm;
+const mapStateToProps = (state) => ({
+  errors: state.errors.session
+});
+
+const mapDispatchToProps = (dispatch) => ({
+  login: (user) => dispatch(login(user)),
+  clearSessionErrors: () => dispatch(clearSessionErrors())
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(LoginForm);
+
 
 
 
