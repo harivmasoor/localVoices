51a6b167793e0a88a2faee49d135c3b401156adc
harivmasoor <harimasoor@gmail.com>
2023-09-17 15:46:34 -0700

    push

--- frontend/src/components/ProfileModal/ProfileModal.js
+++ frontend/src/components/ProfileModal/ProfileModal.js
@@ -1,10 +1,14 @@
 import React, { useContext } from 'react';
 import ReactDOM from 'react-dom';
+import { useSelector } from 'react-redux'; // Import the useSelector hook
 import ModalContext from '../../context/ModalContext'; 
 import './ProfileModal.css';
 
 export function ProfileModal({ onClose, children }) {
   const modalNode = useContext(ModalContext);
+
+  // Use the useSelector hook to get the username from the session or profile state
+  const username = useSelector(state => state.session.user.username); // Assuming the session state has the user details
 
   if (!modalNode) return null;
 
@@ -13,8 +17,10 @@ export function ProfileModal({ onClose, children }) {
       <div id="profile-modal-background" onClick={onClose} />
       <div id="profile-modal-content">
         {children}
+        {username && <a href={`/profile/${username}`} className="view-profile-link">View profile</a>}
       </div>
     </div>,
     modalNode
   );
 }
+
